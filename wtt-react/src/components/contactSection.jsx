85a32cd9c8aed04bfb99504c3939da31cba6e853
jwesinspace <westracompsci@gmail.com>
2019-02-06 07:24:48 -0500

    Adding Content to Home/Contact

--- wtt-react/src/components/contactSection.jsx
+++ wtt-react/src/components/contactSection.jsx
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 class ContactSection extends Component {
 	state = {};
 	render() {
-		return <div id="contactSection">Contact Page</div>;
+		return (<div id="contactSection">
+			<p className="hours">HOURS</p>
+			<ul id="hoursList">
+				<li className="hoursItem">MON-THUR / 11-9</li>
+				<li className="hoursItem">FRI / 11-10</li>
+				<li className="hoursItem">SAT / 12-10</li>
+			</ul>
+			<div id="imageplaceholder"></div>
+			<p>1825 SE 164TH AVE</p>
+			<p>SUITE 101</p>
+			<p>VANCOUVER, WA 98683</p>
+		</div>);
 	}
 }
 
--- wtt-react/src/components/homeSection.jsx
+++ wtt-react/src/components/homeSection.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 class HomeSection extends Component {
 	state = {};
 	render() {
-		return <div id="homeSection">Home Page</div>;
+		return (<div id="homeSection">
+			<p id="callout1">Wild Tiger Thai Restaurant and Bar</p>
+			<p id="calllout2">"Vancouver's Local Thai Experience"</p>
+			<div id="homeimageplaceholder"></div>
+		</div>);
 	}
 }
 
